d2033ee18ef601bdfc2cc9844617b59d0f375c53
AyaanDEV <64188607+Ayaan-Dev@users.noreply.github.com>
2022-07-30 18:11:44 +0530

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ function HomepageHeader() {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container full-height">
         <br />
+        <img className="hero__img" src="/assets/groot-avatar.webp"></img>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>   
